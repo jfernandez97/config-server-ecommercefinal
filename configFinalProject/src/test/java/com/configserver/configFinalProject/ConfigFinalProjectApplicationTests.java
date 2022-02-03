4d79b7aaa42112ef47d23e0dae0e2656b976800d
Juan Fernandez <juan.fernandez@mi.unc.edu.ar>
2022-02-03 14:07:45 -0300

    config project

---
+++ configFinalProject/src/test/java/com/configserver/configFinalProject/ConfigFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configserver.configFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
